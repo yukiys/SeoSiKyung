843327f0425c88873421991eb3878a7d17614575
yukiys <yukihw1218@gmail.com>
2025-08-03 17:36:20 +0900

    enemy anim

--- SeoSiKyung/Assets/Scripts/EnemyMove.cs
+++ SeoSiKyung/Assets/Scripts/EnemyMove.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
-    public Rigidbody2D rd;
-    public SpriteRenderer sr;
-    public Collider2D cd;
-    public Transform player;
+    Rigidbody2D rd;
+    SpriteRenderer sr;
+    Collider2D cd;
+    Transform player;
+    Animator anim;
     public float speed = 2f;
     private bool isAwake = false;
     private bool isMoving = false;
@@ -16,6 +17,7 @@ public class EnemyMove : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         cd = GetComponent<Collider2D>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -24,14 +26,24 @@ public class EnemyMove : MonoBehaviour
         {
             Vector2 direction = (player.position - transform.position).normalized;
             rd.linearVelocity = speed * direction;
-
             sr.flipX = (direction.x < 0);
+            anim.SetBool("isMoving", true);
         }
-        else rd.linearVelocity = Vector2.zero;
+        else
+        {
+            rd.linearVelocity = Vector2.zero;
+            anim.SetBool("isMoving", false);
+        }
+    }
+
+    public void OnHit()
+    {
+        anim.SetBool("isHit", true);
     }
 
     public void WakeUp()
     {
-        
+        isAwake = true;
+        isMoving = true;
     }
 }
