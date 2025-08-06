da160431351ffacab7034ced7c4c707f6a7b4dfe
yukiys <yukihw1218@gmail.com>
2025-08-06 21:21:11 +0900

    DataSet

--- SeoSiKyung/Assets/DataSet/EnemyData.cs
+++ SeoSiKyung/Assets/DataSet/EnemyData.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class EnemyData
 {
     public string name;
@@ -8,6 +9,7 @@ public class EnemyData
     public List<string> resistances;
 }
 
+[System.Serializable]
 public class EnemyDataList
 {
     public List<EnemyData> enemies;
