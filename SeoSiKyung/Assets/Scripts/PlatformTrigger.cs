c6a4660a1bd17b855e301fc3dd65697450c86c0d
yukiys <yukihw1218@gmail.com>
2025-08-08 18:42:55 +0900

    platform trigger

--- SeoSiKyung/Assets/Scripts/PlatformTrigger.cs
+++ SeoSiKyung/Assets/Scripts/PlatformTrigger.cs
@@ -1,9 +1,34 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class PlatformTrigger : MonoBehaviour
 {
-    public EnemyMove[] enemies;
+    public List<EnemyMove> enemies=new List<EnemyMove>();
+    Collider2D cd;
+
+    void Awake()
+    {
+        cd = GetComponent<Collider2D>();
+        FindEnemies();
+    }
+
+    private void FindEnemies()
+    {
+        Bounds bounds = cd.bounds;
+
+        Collider2D[] tracked = Physics2D.OverlapBoxAll(bounds.center, bounds.size, 0f);
+
+        enemies.Clear();
+        foreach (var t in tracked)
+        {
+            EnemyMove enemy = t.GetComponent<EnemyMove>();
+            if (enemy != null)
+            {
+                enemies.Add(enemy);
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
