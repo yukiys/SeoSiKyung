63d54ba8636a56497891c014f41e281e0bf1893e
yukiys <yukihw1218@gmail.com>
2025-08-13 19:34:18 +0900

    Golem Animation, FSM

--- SeoSiKyung/Assets/Scripts/Enemy/EnemyStates/EnemySleepDie2.cs
+++ SeoSiKyung/Assets/Scripts/Enemy/EnemyStates/EnemySleepDie2.cs
@@ -2,20 +2,23 @@ using UnityEngine;
 
 public class EnemySleepDie2 : EnemyState
 {
+    float t, duration = 0.7f;
+    
     public EnemySleepDie2(Enemy enemy, EnemyFSM fsm) : base(enemy, fsm) { }
 
     public override void Enter()
     {
-
+        enemy.rd.linearVelocity = Vector2.zero;
+        enemy.anim.Play("SleepDie2");
+        t = duration;
     }
 
     public override void LogicUpdate()
     {
-
-    }
-
-    public override void PhysicsUpdate()
-    {
-
+        t -= Time.deltaTime;
+        if (t <= 0f)
+        {
+            Object.Destroy(enemy.gameObject);
+        }
     }
 }
